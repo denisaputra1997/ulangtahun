f760a12c912c338566b0ea0d71f475ccc75be20a
denisaputra1997 <deni97.saputra@gmail.com>
2025-05-15 22:15:47 +0700

    update

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,7 +8,7 @@ const Info = () => {
       <h6 className="mb-3">Kami mengundang Bapak/ibu saudara/i untuk hadir di acara yang luar biasa ini</h6>
       <h2 className="mb-3">✨ Detail Acara</h2>
       <p className="fs-6"><strong>📅 Tanggal:</strong> [19 Mei 2025]</p>
-      <p className="fs-6"><strong>📍 Lokasi:<a href="https://maps.app.goo.gl/SbTVUh3N1kAPsjXR8" target="_blank" rel="noopener noreferrer" className="text-dark">  [Jl. Flamboyan Blok A16 No.24 (klick!!!)]</a></strong></p>
+      <p className="fs-6"><strong>📍 Lokasi:<a href="https://maps.app.goo.gl/SbTVUh3N1kAPsjXR8" target="_blank" rel="noopener noreferrer" className="text-dark">  [Jl. Flamboyan Blok A16 No.24] (klik!!!)</a></strong></p>
       <p className="fs-6"><strong>⏰ Waktu:</strong> [19.00 WIB]</p>
     </section>
   );
